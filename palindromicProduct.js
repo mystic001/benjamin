113427af97438a282c53082ae28f86b60b02a6cb
Bamidele Emmanuel <bamideleomonayin@gmail.com>
2022-04-08 09:54:51 +0100

    second question

--- palindromicProduct.js
+++ palindromicProduct.js
@@ -1,57 +1,84 @@
 const palindromeReturn = (numb1, numb2) => {
     const arraya = range(numb1, numb2)
-
-    console.log(arraya)
-    const newArr = []
+    const palArray = []
     for (let i = 0; i < arraya.length; i++) {
-        newArr.push(arraya[i]*arraya[i])
+        //This step is for a number to multiply itsef
+        const res = Math.pow(arraya[i], 2);
+        if (palindrome(res.toString())) {
+            palArray.push(arraya[i])
+        }
 
         for (let j = i + 1; j <= arraya.length - 1; j++) {
-            const result = arraya[i] * arraya[j]
-            newArr.push(result)
+
+            //If the products of two numbers result in a palindrome enter the block of code below 
+            if (palindrome((arraya[i] * arraya[j]).toString())) {
+                palArray.push(arraya[i])
+                palArray.push(arraya[j])
+            }
+
         }
 
     }
 
-    // const first = arraya[0]*arraya[0]
-    // newArr.unshift(first)
-    // const last = arraya[arraya.length-1]*arraya[arraya.length-1]
-    // newArr.push(last);
+    const unique = uniqueArray(palArray)
+    const value = [unique[0], unique[unique.length - 1]]
+    console.log("values with palindrome are "+value)
     
-
-    const palArray = []
-    for(let i = 0 ; i < newArr.length ; i++){
-        console.log(newArr[i])
-        if(newArr[i].toString().length === 1){
-             palArray.push(newArr[i])
-        } else if(palindrome(newArr[i].toString())){
-        palArray.push(newArr[i])
-        }else{
-            continue
-        }
+    console.log("There products are below")
+    for (let i = 0; i < value.length; i++) {
+        const arr = factors(value[i])
+        console.log(sortArray(arr))
     }
 
-    console.log(palArray)
 
 }
 
+
+//Function to generate an array
 function range(start, end) {
     return Array(end - start + 1)
         .fill().map((_, idx) => start + idx)
 }
 
 
+//Function for identifying palindrome
 function palindrome(str) {
-    var len = str.length;
-    var mid = Math.floor(len/2);
+    return str === str.split('').reverse().join('')
+}
 
-    for ( var i = 0; i < mid; i++ ) {
-        if (str[i] !== str[len - 1 - i]) {
-            return false;
+
+
+//Function to remove duplicates
+function uniqueArray(arr) {
+    var a = [];
+    for (var i = 0, l = arr.length; i < l; i++)
+        if (a.indexOf(arr[i]) === -1 && arr[i] !== '')
+            a.push(arr[i]);
+    return a;
+}
+
+//Function to sort array
+function sortArray(arr) {
+    return arr.sort((a, b) => a - b)
+}
+
+
+//Function to get the factors of a number 
+function factors(n) {
+
+    var result = [];
+
+    for (var i = 1; i <= Math.sqrt(n); i++) {
+        if (n % i === 0) {
+            result.push(i);
+            if (i !== n / i) {
+                result.push(n / i);
+            }
         }
     }
 
-    return true;
+    return result;
 }
 
-palindromeReturn(10, 15)
+
+palindromeReturn(20, 80)
