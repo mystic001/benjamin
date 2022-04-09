02835ed66c11bf2cb66822c00f75442c6ee6a967
Bamidele Emmanuel <bamideleomonayin@gmail.com>
2022-04-09 20:48:27 +0100

    done the last part

--- structurednumb.js
+++ structurednumb.js
@@ -4,21 +4,22 @@ var tens = "twenty thirty forty fifty sixty seventy eighty ninety".split(" ");
 
 const formatedNumber = (number) => {
 
-    const numbInWords = number2words(number);
-    const formatedNum = numberWithCommas(number);
-
-    console.log(`The number is ${numbInWords} and it is formated as ${formatedNum}`)
-    return numbInWords + " " + formatedNum;
+    if(number.toString().length > 12){
+        console.log("Number is too large")
+    }else{
+        const numbInWords = number2words(number);
+        const formatedNum = numberWithCommas(number);
+        console.log(`The number is ${numbInWords} and it is formated as ${formatedNum}`)
+        
+    }
+  
 }
 function number2words(n){
-
-    console.log(num)
-    console.log(tens)
     if (n < 20) return num[n];
     var digit = n%10;
     if (n < 100) return tens[Math.floor((n/10)-2)] + (digit? "-" + num[digit]: "");
     if (n < 1000) return num[Math.floor((n/100))] +" hundred" + (n%100 == 0? "": " and " + number2words(n%100));
-    return number2words(~~(n/1000)) + " thousand" + (n%1000 != 0? " " + number2words(n%1000): "");
+    return number2words(Math.floor((n/1000))) + " thousand" + (n%1000 != 0? " " + number2words(n%1000): "");
 }
 
 
@@ -26,4 +27,4 @@ function numberWithCommas(number) {
     return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
-formatedNumber(999);
+formatedNumber(9998888888888);
